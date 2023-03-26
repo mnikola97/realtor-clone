1b47b8cbb3eb38212625cc5c5cb1d4dc9c7f0a56
mnikola97 <78258230+mnikola97@users.noreply.github.com>
2023-03-26 21:38:22 +0200

    added pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
+import { BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import ForgotPassword from "./pages/ForgotPassword";
+import Home from "./pages/Home";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignOut";
 function App() {
   return (
-    <h1>Hello World!</h1>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/offers" element={<Offers/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/sign-in" element={<SignIn/>}/>
+        <Route path="/sign-out" element={<SignOut/>}/>
+        <Route path="/forgot-password" element={<ForgotPassword/>}/>
+      </Routes>
+    </Router>
   );
 }
 
